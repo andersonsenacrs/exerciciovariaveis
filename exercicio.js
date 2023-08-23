218922da1fd06336ffbec7d8de3be2ce7dbb6540
andersonsenacrs <142456849+andersonsenacrs@users.noreply.github.com>
2023-08-22 21:45:54 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,26 @@
+/*
+* Anderson Miguel
+*
+*/
+
+// 1. resposta
+//exercicios variaveis
+//1. irá ler 10 + 5
+
+// 2. resposta
+//console.log(10, 10, 10)
+
+// 3. resposta
+let horasTrabalhadas;
+let diaria;
+
+// 3. resposta - Exercícios de escrita de código
+
+let nome;
+let idade;
+
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Qual é a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+// Foi impresso devido o questionamento ao usuário pedindo seus dados como Nome e Idade "no primeiro momento"
